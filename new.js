125e9d6dde130e795972c6a0fc0040caf75a8f67
Rohit Soni <soni2000rohit@gmail.com>
2024-04-02 12:41:36 +0530

    Initial commit

---
+++ new.js
@@ -0,0 +1,21 @@
+function scrollAbout(){
+    window.scrollTo(700,700);
+}
+
+function scrollEligiblity(){
+    window.scrollTo(850,850);
+}
+
+function scrollDetails(){
+    window.scrollTo(950,950);
+}
+
+function scrollFAQs(){
+    window.scrollTo(1200,1200);
+}
+
+var typed = new Typed('#element', {
+    strings: ['English-Speaking', 'Computer Knowledge', 'and many more courses for free...'],
+    typeSpeed: 50,
+});
+
